1f17cc27be5056d55de23254126441238b146756
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-04-18 09:23:18 +0200

    DataContext 1.0

---
+++ WebAPI/DataContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace WebAPI
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<TeacherModel> teachers { get; set; }
+        public DbSet<LessonModel> lessons { get; set; }
+        public DbSet<StudentModel> students { get; set; }
+        private readonly IConfiguration _configuration;
+
+        readonly string connectionString;
+        // Reaseon: Properties need to make datebase, but we never use them in code.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable. 
+        public DataContext(IConfiguration configuration)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _configuration = configuration;
+#if DEBUG
+#pragma warning disable CS8601 // Possible null reference assignment.
+            connectionString = _configuration.GetConnectionString("LocalDB");
+#pragma warning restore CS8601 // Possible null reference assignment.
+#else
+#pragma warning disable CS8601 // Possible null reference assignment.
+            connectionString = _configuration.GetConnectionString("MyAspDB");
+#pragma warning restore CS8601 // Possible null reference assignment.
+#endif
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<TeacherModel>()
+                .HasOne<LessonModel>(r => r.oil)
+                .WithMany(o => o.recipes);
+
+            modelBuilder
+                .Entity<LessonModel>()
+                .HasOne<StudentModel>(r => r.oil)
+                .WithMany(o => o.recipes);
+        }
+    }
+}
---
+++ WebAPI/Extensions/ControllerBaseExtension.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Extensions
+{
+    public static class ControllerBaseExtension
+    {
+        public static IActionResult Run(this ControllerBase controller, Func<IActionResult> action)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    LogToFile(ex);
+                }
+                catch { }
+#if DEBUG
+                return controller.StatusCode(500, new
+                {
+                    error = ex.InnerException == null ? ex.GetType().Name : ex.InnerException.GetType().Name,
+                    stackTrace = ex.StackTrace
+                });
+#else
+                return controller.StatusCode(500, new
+                {
+                    error = ex.InnerException == null ? ex.GetType().Name : ex.InnerException.GetType().Name,
+                });
+
+#endif
+            }
+        }
+
+        private static string? path = null;
+        private static object lockLogFile = new object();
+
+        private static void LogToFile(Exception ex)
+        {
+            //source: https://code-maze.com/aspnetcore-read-appsettings-values-from-a-json-file/
+
+            if (path == null)
+            {
+                var configuration = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+
+                path = configuration.GetValue<string>("Logfile:Path");
+            }
+            var fileName = Path.Combine(path, DateTime.Today.Year.ToString());
+            if (!Directory.Exists(fileName))
+                Directory.CreateDirectory(fileName);
+            fileName = Path.Combine(fileName, DateTime.Today.Month.ToString());
+            if (!Directory.Exists(fileName))
+                Directory.CreateDirectory(fileName);
+            fileName = Path.Combine(fileName, $"WebApiLog_{DateTime.Today:yyyy-MM-dd}.log");
+
+            lock (lockLogFile)
+            {
+                using (var sw = new StreamWriter(fileName, true))
+                {
+                    sw.WriteLine($"{DateTime.Now:HH:mm:ss}\t{ex.Message}");
+                    sw.WriteLine($"\t\t{ex.StackTrace?.Replace("\n", "\n\t\t")}");
+                }
+            }
+
+        }
+    }
+}
